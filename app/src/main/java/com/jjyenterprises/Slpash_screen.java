c8562509de67bd531bbf7cc8535d2d2aedd92abd
SakuraGlory <96517196+SakuraGlory@users.noreply.github.com>
2023-05-24 13:44:39 +0530

    init

---
+++ app/src/main/java/com/jjyenterprises/Slpash_screen.java
@@ -0,0 +1,14 @@
+package com.jjyenterprises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Slpash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/jjyenterprises/authentication/Login.java
@@ -0,0 +1,16 @@
+package com.jjyenterprises.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jjyenterprises.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
